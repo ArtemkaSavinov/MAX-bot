427d47a0607c4368e05fc23c9e6d7808d983eeb4
nikit <nikita513474@gmail.com>
2025-11-14 23:17:55 +0300

    tasksMenu kb

--- handlers/actions.js
+++ handlers/actions.js
@@ -1,7 +1,16 @@
 import {userState} from '../state/userState.js'
 import { Task } from '../models/Task.js';
+import { startKb } from '../keyboards/tasksMenu.js';
+import { middleKb } from '../keyboards/tasksMenu.js';
+import { lastKb } from '../keyboards/tasksMenu.js';
+import {mainMenu} from '../keyboards/mainMenu.js'
 
 export function setupActions(bot) {
+	bot.action('main-menu', async (ctx) => {
+		ctx.reply('Главное меню:', {attachments: [mainMenu]})
+		userState[ctx.message.sender.user_id] = {};
+	});
+
 	bot.action('add-task', async (ctx) => {
 		const userId = ctx.callback.user.user_id;
 		userState[userId] = { step: 'title', task: {} };
@@ -20,16 +29,18 @@ export function setupActions(bot) {
 			}
 
 			// Формируем текст сообщения
-			const taskList = tasks
-				.map((task, index) => {
+			else{
+			
+			const taskList = tasks.map((task, index) => {
 					return `${index + 1}. ${task.title} (Статус: ${task.status}, Дедлайн: ${task.deadline}, Сложность: ${task.difficulty}, Категория: ${task.category})`;
 				})
-				.join('\n');
+				.join('\n\n');
 
 			const message = `📋 *Ваши задачи:*\n\n${taskList}`;
 
+			}
 			// Отправляем с форматированием Markdown
-			await ctx.reply(message, { format: 'markdown' });
+			await ctx.reply(message, { format: 'markdown' }, {attachments: [mainMenu]});
 
 		} catch (error) {
 			console.error('Ошибка при получении задач:', error);
