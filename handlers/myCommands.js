f525f6000eb11d5e9c4c115f0b393c4a0558d85a
nikit <nikita513474@gmail.com>
2025-11-15 04:23:41 +0300

    beta 1.0.1, myCommands extended

--- handlers/myCommands.js
+++ handlers/myCommands.js
@@ -1,4 +1,8 @@
 export const myCommands = [
 	{ name: 'start', description: 'Стартовая команда' },
-	{ name: 'mainMenu', description: 'К главному меню'}
+	{ name: 'main-menu', description: 'К главному меню'},
+	{ name: 'admin-add-task', description: 'Быстрое добавление задачи' },
+	{ name: 'admin-add-tasks', description: 'Добавить 25 задач' },
+	{ name: 'admin-clear-tasks', description: 'Удалить все задачи' },
+
 ]
